921d094e6444c029762bb54a03a9d9d234de89db
Martyshina Tatiana <taniadrozd12@gmail.com>
2024-02-18 11:20:14 +0300

    first commit

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,54 @@
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class PersonTest {
+
+    public void currentTime() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+        }
+
+        @BeforeClass
+        void BeforeClass() {
+            System.out.println("Before Class");
+            currentTime();
+        }
+
+        @AfterClass
+        void AfterClass() {
+            System.out.println("After Class");
+            currentTime();
+        }
+
+        @DataProvider(name = "ageDataProvider")
+        Object[][] ageDataProvider() {
+            return new Object[][]{
+                    {0, false},
+                    {10, false},
+                    {13, true},
+                    {14, true},
+                    {18, true},
+                    {19, true},
+                    {20, false},
+                    {50, false}
+            };
+        }
+
+        @Test(dataProvider = "ageDataProvider")
+        public void Test(int age, boolean expectedResult) {
+            boolean result = Person.isTeenager(age);
+            System.out.println(result);
+            assertEquals(result, expectedResult);
+        }
+
+    }
